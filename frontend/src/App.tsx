d81bc2853941254fef42fcb797f80e7f399a84f2
Ebele Lynda Okolo <okolo.ebelelynda@gmail.com>
2026-01-16 04:30:50 +0100

    Remove unused Page import from App component
    Removed unused import for Page component.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { Swap } from './pages/Swap'
 import { Landing } from './pages/Landing'
-import { Page } from '.pages/page'
 
 export default function App() {
   const [route, setRoute] = useState<'landing' | 'swap'>('swap')
