d26346f5279a950d081f869fbbb826574aa9404e
Ebele Lynda Okolo <okolo.ebelelynda@gmail.com>
2026-01-16 04:30:31 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Swap } from './pages/Swap'
 import { Landing } from './pages/Landing'
+import { Page } from '.pages/page'
 
 export default function App() {
   const [route, setRoute] = useState<'landing' | 'swap'>('swap')
